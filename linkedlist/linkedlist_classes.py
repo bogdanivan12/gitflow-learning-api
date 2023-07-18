92b4ec935eb90fd36a53f4db5999580afc204dfe
Bogdan Ivan <bogdani@adobe.com>
2023-07-19 00:23:35 +0300

    Added delete-linked-list endpoint. Lists are now identified by name, not by id. Added serialisation for the linked_list_dict via pickle. Docker file created

--- linkedlist/linkedlist_classes.py
+++ linkedlist/linkedlist_classes.py
@@ -4,26 +4,49 @@ File containing the classes needed for a linked list implementation.
 
 
 class Node:
-    def __init__(self, value=None, nextNode=None):
+    """
+    Class that represents a node in a linked list.
+
+    Object attributes:
+        value: the actual value of the node.
+        next_node = reference to the next node in the linked list.
+    """
+    def __init__(self, value=None, next_node=None):
+        """
+        Function that initializes the object with the provided parameters.
+        """
         self.value = value
-        self.next = nextNode
+        self.next = next_node
 
     def __str__(self):
+        """
+        Function that defines the string cast for the Node objects.
+        """
         return str(self.value)
 
 
 class LinkedList:
-    nextId = 0
+    """
+    Class that represents a linked list.
 
-    def __init__(self, head=None):
+    Object attributes:
+        head: the first element (node) in the list.
+        name: the name of the list.
+    """
+    def __init__(self, name, head=None):
+        """
+        Function that initializes the object with the provided parameters.
+        """
         self.head = head
-        self.id = LinkedList.nextId
-        LinkedList.nextId += 1
+        self.name = name
 
     def __str__(self):
+        """
+        Function that defines the string cast for the Node objects.
+        """
         response = str(self.head)
-        aux = self.head.next
+        aux = self.head.next_node
         while aux is not None:
             response += f" {aux}"
-            aux = aux.next
+            aux = aux.next_node
         return response
