c1642b481666cf95fb98fdb6be95c08a6df356d4
Bogdan Ivan <bogdani@adobe.com>
2023-07-21 11:56:29 +0300

    Modified path in build_runner.sh

--- build_runner.sh
+++ build_runner.sh
@@ -4,7 +4,7 @@ set -x
 
 SCRIPT_PATH=$(dirname "$0")
 
-python3 -m pylint . \
+python3 -m pylint gitflow_learning_api \
   --init-hook="import sys; sys.path.append('$SCRIPT_PATH')" \
   --rcfile=.pylintrc
 
