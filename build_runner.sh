af2dab76152c1043ea01feaad341b91558e6d57b
Bogdan Ivan <bogdani@adobe.com>
2023-07-21 17:16:23 +0300

    Added start_service function into build_runner.sh

--- build_runner.sh
+++ build_runner.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+start_service () {
+  docker stop "$1" && docker rm "$1"
+
+  docker rmi "$1"
+
+  docker build -f gitflow_learning_api/"$1"/Dockerfile_"$1" -t "$1" .
+
+  if [ "$1" = "linkedlist" ]
+  then
+    service_port=8080
+  else
+    service_port=8081
+  fi
+
+  docker run -p "$service_port":"$service_port" --name "$1" "$1" &
+}
+
 set -x
 
 SCRIPT_PATH=$(dirname "$0")
@@ -10,20 +27,23 @@ python3 -m pylint gitflow_learning_api \
 
 python3 -m pytest --disable-warnings
 
-
-docker stop linkedlist && docker rm linkedlist
-
-docker rmi linkedlist
-
-docker build -f gitflow_learning_api/linkedlist/Dockerfile_linkedlist -t linkedlist .
-
-docker run -p 8080:8080 --name linkedlist linkedlist &
+start_service linkedlist
+start_service stringproc
 
 
-docker stop stringproc && docker rm stringproc
+#docker stop linkedlist && docker rm linkedlist
+#
+#docker rmi linkedlist
+#
+#docker build -f gitflow_learning_api/linkedlist/Dockerfile_linkedlist -t linkedlist .
+#
+#docker run -p 8080:8080 --name linkedlist linkedlist &
 
-docker rmi stringproc
 
-docker build -f gitflow_learning_api/stringproc/Dockerfile_stringproc -t stringproc .
-
-docker run -p 8081:8081 --name stringproc stringproc &
+#docker stop stringproc && docker rm stringproc
+#
+#docker rmi stringproc
+#
+#docker build -f gitflow_learning_api/stringproc/Dockerfile_stringproc -t stringproc .
+#
+#docker run -p 8081:8081 --name stringproc stringproc &
